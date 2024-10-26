6190042cdf8c8736ea2babf62b2265bfdd9d1afe
0hyunCho <yhcho.inspien@co.kr>
2024-10-27 04:13:34 +0900

    sudo

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # 기존 애플리케이션 종료
-pkill -9 -f 'java -jar /home/ec2-user/CICD_TEST/elice-0.0.1-SNAPSHOT.jar'
+sudo pkill -9 -f 'java -jar /home/ec2-user/CICD_TEST/elice-0.0.1-SNAPSHOT.jar'
